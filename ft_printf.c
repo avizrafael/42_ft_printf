d4815d2d5a146f728ed34e7b5b7b02123d0aaf1d
Rafael <rafael.aviz@gmail.com>
2024-01-11 19:56:57 +0000

    commit

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: raviz-es <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/07 14:30:41 by raviz-es          #+#    #+#             */
-/*   Updated: 2024/01/07 17:08:20 by raviz-es         ###   ########.fr       */
+/*   Updated: 2024/01/11 09:53:46 by raviz-es         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -75,24 +75,23 @@ int	ft_check(const char *format, va_list ap)
 	int		len;
 
 	len = 0;
-	while (*format)
-	{
+	
 		if (*format == 'c')
-			len += ft_print_c(va_arg(ap, int));
+			len = ft_print_c(va_arg(ap, int));
 		else if (*format == 's')
-			len += ft_print_s(va_arg(ap, char *));
+			len = ft_print_s(va_arg(ap, char *));
 		else if (*format == 'd' || *format == 'i')
-			len += ft_print_d(va_arg(ap, int));
+			len = ft_print_d(va_arg(ap, int));
 		else if (*format == 'p')
-			len += ft_print_p(va_arg(ap, U_LI));
+			len = ft_print_p(va_arg(ap, U_LI));
 		else if (*format == 'x' || *format == 'X')
-			len += ft_print_x(va_arg(ap, unsigned int));
+			len = ft_print_x(va_arg(ap, unsigned int));
 		else if (*format == '%')
-			len += (int)write(1, "%", 1);
+			len = (int)write(1, "%", 1);
 		else
-			len += (int)write(1, format, 1);
-		format++;
-	}
+			len = (int)write(1, format, 1);
+		
+	
 	return (len);
 }
 
@@ -110,10 +109,13 @@ int	ft_printf(const char *format, ...)
 			len += ft_check(++format, ap);
 		}
 		else
+		{
 			len += (int)write(1, format, 1);
+		}
 		format++;
 	}
 	va_end(ap);
+	printf(" %d\n", len);
 	return (len);
 }
 
@@ -144,6 +146,6 @@ int	main(void)
 	ft_printf("%X\n", number4);
 	//ft_printf("%x %X\n", number3, number4);
 	ft_printf("%%\n");
-	ft_printf("%s %d\n", string, number1);
+	ft_printf("%c %c\n", byte, byte);
 	return (0);
 }
